a9811d56ac9a5ebb25d78ad28d14f13bc94507e4
Shahil Raj <shahilr641@gmail.com>
2024-01-13 23:32:12 +0530

    add code files

---
+++ script.js
@@ -0,0 +1,28 @@
+// Selecting all the required classes from HTML to change 
+var body = document.body
+var check = document.querySelector('#check')
+var box = document.querySelector('.box')
+var ball = document.querySelector('.ball')
+
+// Adding an eventListener function to change color everytime var check is changed.(checked & unchecked)
+
+check.addEventListener('change',function(){
+
+  //   conditions to apply when checkbox is checked
+
+  if(this.checked == true){
+    box.setAttribute('style','background-color:#222222;')
+    ball.setAttribute('style','transform:translatex(100%);')
+    body.setAttribute('style','background-color:black; color:white;') 
+
+  }
+
+  //   conditions to apply when checkbox is unchecked
+
+  if(this.checked == false){
+    box.setAttribute('style','background-color:#222222; color:white;')
+    ball.setAttribute('style','transform:translatex(0%);')
+    body.setAttribute('style','background-color:white; color:#222222;')
+
+  }
+})
